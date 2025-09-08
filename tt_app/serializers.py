cc5b471e29dd6ad311c5b4dfc645ddc6cca188c6
Levester Chaney <levester.chaney@gmail.com>
2025-09-07 20:11:34 -0700

    fill in missing logic for create and get of timesheet and lineitem endpoints

--- tt_app/serializers.py
+++ tt_app/serializers.py
@@ -9,6 +9,32 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["username", "email"]
 
 
+class TimesheetCreateRequestSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    description = serializers.CharField()
+    rate = serializers.FloatField()
+
+    def create(self, validated_data):
+        return Timesheet.objects.create(
+            user_id=validated_data["user_id"],
+            description=validated_data["description"],
+            rate=validated_data["rate"]
+        )
+
+
+class LineItemCreateRequestSerializer(serializers.Serializer):
+    timesheet_id = serializers.IntegerField()
+    date = serializers.DateTimeField()
+    minutes = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return LineItem.objects.create(
+            timesheet_id=validated_data["timesheet_id"],
+            date=validated_data["date"],
+            minutes=validated_data["minutes"]
+        )
+
+
 class LineItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = LineItem
@@ -20,4 +46,4 @@ class TimesheetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Timesheet
-        fields = ["id", "description", "created_at", "entries"]
+        fields = ["id", "description", "rate", "entries", "created_at"]
--- tt_app/views.py
+++ tt_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from rest_framework import viewsets, status
 from .models import Timesheet, LineItem
-from .serializers import UserSerializer, TimesheetSerializer, LineItemSerializer
+from .serializers import UserSerializer, TimesheetSerializer, LineItemSerializer, TimesheetCreateRequestSerializer, LineItemCreateRequestSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -16,13 +16,13 @@ class TimesheetViewSet(viewsets.ModelViewSet):
     serializer_class = TimesheetSerializer
 
     def get_queryset(self):
-        user_id = self.request.data["userId"]
+        user_id = self.request.data["user_id"]
         return Timesheet.objects.filter(user_id=user_id)
 
     def create(self, request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = TimesheetCreateRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save()
         return HttpResponse(serializer.data, status=status.HTTP_201_CREATED)
 
 
@@ -30,11 +30,11 @@ class LineItemViewSet(viewsets.ModelViewSet):
     serializer_class = LineItemSerializer
 
     def get_queryset(self):
-        timesheet_id = self.request.data["timesheetId"]
+        timesheet_id = self.request.data["timesheet_id"]
         return LineItem.objects.filter(timesheet_id=timesheet_id)
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = LineItemCreateRequestSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save()
         return HttpResponse(serializer.data, status=status.HTTP_201_CREATED)
